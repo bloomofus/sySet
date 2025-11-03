d48b4942b2f88f4218a30c6636a0b622939e3b56
mzz <3108482097@qq.com>
2025-11-03 16:38:27 +0800

    2025.11.03 添加
    使得git配置更加简单，增加搜狗输入法在ubuntu2404的配置方法
    - new file:   软件设置/Git/git_init.sh
    - new file:   软件设置/搜狗输入法/搜狗输入法.md

---
+++ 软件设置/Git/git_init.sh
@@ -0,0 +1,30 @@
+ sudo apt install -y git
+ 
+ # add global user
+ git config --global user.email "3108482097@qq.com"
+ git config --global user.name "mzz"
+ 
+ # add chinese support
+ git config --global core.quotepath false
+ 
+ # change default editor to nano
+ git config --global core.editor "nano"
+ 
+ # gen ssh pubkey
+ ssh-keygen -t ed25519 -C "3108482097@qq.com"
+ 
+cat >> ~/.bashrc << 'EOF'
+
+# >>> 自定义git快捷命令 >>>
+alias gi='git init'
+alias gii='touch .gitignore'
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit'
+alias gpp='git push'
+alias gp='git pull'
+alias gb='git branch'
+# <<< 自定义git快捷命令 <<<
+
+EOF
